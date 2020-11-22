53e8bf94890698b0727309e3b5d101709588fdee
Christopher Weems <hello@christopherweems.com>
2020-11-22 00:51:55 -0500

    Add `Malaysia` college group

---
+++ Sources/Collegiate/Malaysia.swift
@@ -0,0 +1,17 @@
+//
+//  Malaysia.swift
+//  
+//
+//  Created by Christopher Weems on 11/22/20.
+//
+
+import Foundation
+
+struct Malaysia: CollegeGroup {
+    var body: Body {
+        "German-Malaysian Institute"
+            .url(authority: "gmi.edu.my")
+        
+    }
+    
+}
