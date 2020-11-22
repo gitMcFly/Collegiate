790a4b448edcc57f57dad88a62cf934a8656ba30
Christopher Weems <hello@christopherweems.com>
2020-11-21 23:58:07 -0500

    Add `MixedGroup[..]` property accessor subscript `MixedGroup.init(elements:)` is now public

--- Sources/MixedGroup/MixedGroup.swift
+++ Sources/MixedGroup/MixedGroup.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct MixedGroup<Element> {
     private var nodes: [Node]
     
-    internal init(elements: [Element]) {
+    public init(elements: [Element]) {
         nodes = elements.map { Node.element($0) }
         
     }
@@ -28,6 +28,24 @@ public struct MixedGroup<Element> {
 }
 
 public extension MixedGroup {
+    struct SubscriptKeys {
+        public var elements: [Element]
+        
+    }
+    
+    subscript<V>(keyPath: KeyPath<SubscriptKeys, V>) -> [Element] {
+        switch keyPath {
+        case \SubscriptKeys.elements:
+            return nodes.flatMap(\.elements)
+            
+        default:
+            fatalError()
+        }
+    }
+    
+}
+
+public extension MixedGroup {
     init(@MixedGroupBuilder<Element> _ builder: () -> MixedGroup) {
         self = builder()
     }
@@ -39,9 +57,23 @@ public extension MixedGroup {
 
 private extension MixedGroup {
     enum Node {
-        case element(_ college: Element)
+        case element(_ element: Element)
         case subgroup(_ group: MixedGroup)
         
+    }
+    
+}
+
+private extension MixedGroup.Node {
+    var elements: [Element] {
+        switch self {
+        case .element(let element):
+            return [element]
+            
+        case .subgroup(let subgroup):
+            return subgroup.nodes.flatMap(\.elements)
+            
+        }
     }
     
 }
