3feabbf4bfd6da8cecee227aa250a2e591dae7a2
Christopher Weems <hello@christopherweems.com>
2020-11-22 00:00:31 -0500

    Add `CollegeGroup.state(_:)` property setter

--- Sources/Collegiate/Protocols/CollegeGroup.swift
+++ Sources/Collegiate/Protocols/CollegeGroup.swift
@@ -7,6 +7,8 @@
 
 import Foundation
 import MixedGroup
+import Statehood
+import unstandard
 
 public protocol CollegeGroup {
     typealias Body = Group
@@ -19,6 +21,44 @@ public protocol CollegeGroup {
 extension MixedGroup: CollegeGroup where Element == College {
     public var body: Self {
         return self
+    }
+    
+}
+
+extension ForEach: CollegeGroup where Data.Element == College {
+    public var body: Body {
+        return .init(elements: Array(data))
+    }
+    
+}
+
+
+// MARK: - Property Setters
+
+extension CollegeGroup {
+    func state(_ state: State) -> some CollegeGroup {
+        AnyCollegeGroup {
+            ForEach(body[\.elements]) { college in
+                college.state(state)
+                
+            }
+        }
+    }
+    
+}
+
+
+// MARK: -
+
+fileprivate struct AnyCollegeGroup: CollegeGroup {
+    @Memoized var body: Body
+    
+    init(@CollegeGroupBuilder _ builder: @escaping () -> CollegeGroup) {
+        _body = Memoized { builder().body }
+    }
+    
+    init<CG: CollegeGroup>(other: CG) {
+        _body = Memoized { other.body }
     }
     
 }
