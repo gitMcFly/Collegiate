2a37fff861555cc1a71b4badae961525c2102260
Christopher Weems <hello@christopherweems.com>
2020-11-29 22:44:52 -0500

    Add `CollegiateTests.testCollegesHaveCities`

--- Tests/CollegiateTests/CollegiateTests.swift
+++ Tests/CollegiateTests/CollegiateTests.swift
@@ -1,10 +1,13 @@
 import XCTest
+import unstandard
+@testable import CollegeGroup
 @testable import Collegiate
 @testable import PartialAddress
 
 final class CollegiateTests: XCTestCase {
     static var allTests = [
         ("testCountryTitle", testCountryTitle),
+        ("testCollegesHaveCities", testCollegesHaveCities),
     ]
     
 }
@@ -16,6 +19,18 @@ extension CollegiateTests {
         
         let australia = Country.australia
         XCTAssertEqual(australia.formattedTitle, "Australia")
+        
+    }
+    
+    func testCollegesHaveCities() {
+        AnyCollegeGroup.allCases
+            .flatMap(\.body[\.elements])
+            .forEach {
+                let hasCity = $0[\.address.city] != nil
+                let isMissingURL = $0[\.homePage] == nil && $0[\.siteAuthority] == nil
+                
+                XCTAssert(hasCity || isMissingURL, "\($0[\.title]) must have a city")
+            }
         
     }
     
