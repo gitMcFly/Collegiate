bf8d305137ae0320beb512cd0225f8d6964691d0
Christopher Weems <hello@christopherweems.com>
2020-11-29 20:20:29 -0500

    Add `UniversitySystem.title` protocol extension method

--- Sources/CollegeGroup/Protocols/UniversitySystem.swift
+++ Sources/CollegeGroup/Protocols/UniversitySystem.swift
@@ -5,11 +5,23 @@
 //  Created by Christopher Weems on 11/22/20.
 //
 
+import Algorithms
 import Foundation
 import unstandard
 
 public protocol UniversitySystem: Identifiable {
     var title: String { get }
     @UUIDResult var id: UUID { get }
+    
+}
+
+public extension UniversitySystem {
+    var title: String {
+        "\(self)"
+            .chunked { $0.isUppercase || $1.isLowercase }
+            .map(\.capitalized)
+            .replacingOccurrences(of: "Of", with: "of")
+            .joined(separator: " ")
+    }
     
 }
