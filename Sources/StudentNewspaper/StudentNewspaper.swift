bf6d58d250e91470250397d3dd414c3bdfb4dd83
Christopher Weems <hello@christopherweems.com>
2020-11-29 23:28:16 -0500

    Add `StudentNewspaper.siteReference`

--- Sources/StudentNewspaper/StudentNewspaper.swift
+++ Sources/StudentNewspaper/StudentNewspaper.swift
@@ -5,9 +5,24 @@
 //  Created by Christopher Weems on 11/29/20.
 //
 
+import unstandard
+
 public enum StudentNewspaper {
     case dailyBeacon
+    case dailyTexan
     case msuExponent
     case triangle
+    
+}
+
+public extension StudentNewspaper {
+    @SingleResult var siteReference: SiteReference? {
+        switch self {
+        case .dailyBeacon: .authority("utdailybeacon.com")
+        case .dailyTexan: .authority("dailytexanonline.com")
+        case .msuExponent: .authority("msuexponent.com")
+        case .triangle: .authority("thetriangle.org")
+        }
+    }
     
 }
