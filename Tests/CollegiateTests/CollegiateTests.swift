7c701e2d8816ad6dde894fe41edbf089820bd4fd
Christopher Weems <hello@christopherweems.com>
2020-11-29 23:45:26 -0500

    Add `CollegiateTests.testStudentNewspaperURLFormatting

--- Tests/CollegiateTests/CollegiateTests.swift
+++ Tests/CollegiateTests/CollegiateTests.swift
@@ -3,11 +3,13 @@ import unstandard
 @testable import CollegeGroup
 @testable import Collegiate
 @testable import PartialAddress
+@testable import StudentNewspaper
 
 final class CollegiateTests: XCTestCase {
     static var allTests = [
         ("testCountryTitle", testCountryTitle),
         ("testCollegesHaveCities", testCollegesHaveCities),
+        ("testStudentNewspaperURLFormatting", testStudentNewspaperURLFormatting),
     ]
     
 }
@@ -30,6 +32,23 @@ extension CollegiateTests {
                 let isMissingURL = $0[\.homePage] == nil && $0[\.siteAuthority] == nil
                 
                 XCTAssert(hasCity || isMissingURL, "\($0[\.title]) must have a city")
+            }
+        
+    }
+    
+    func testStudentNewspaperURLFormatting() {
+        StudentNewspaper.allCases
+            .compactMap(\.siteReference)
+            .forEach {
+                switch $0 {
+                case .authority(let authority, _):
+                    XCTAssertFalse(authority.hasPrefix("http"), "\(authority) is malformed")
+                    
+                case .webPage(let pageURL):
+                    XCTAssert(pageURL.hasPrefix("http"), "\(pageURL) is malformed")
+                    
+                }
+                
             }
         
     }
