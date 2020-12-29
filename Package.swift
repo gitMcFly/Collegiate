38e0231016009c23a226e408576997578c2f069e
Christopher Weems <hello@christopherweems>
2020-12-29 02:57:44 -0500

    Update minimum `Resultto` package version to "0.0.2"

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/christopherweems/Resultto", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/christopherweems/Resultto", .upToNextMajor(from: "0.0.2")),
         .package(url: "https://github.com/gitMcFly/MixedGroup", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/christopherweems/Statehood", .upToNextMajor(from: "0.0.1")),
         .package(url: "https://github.com/christopherweems/unstandard", .upToNextMajor(from: "0.0.6")),
