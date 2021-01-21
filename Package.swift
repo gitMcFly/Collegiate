b7ded8b7417a508d2966d5cd67a3f3e6656a9067
Christopher Weems <hello@christopherweems>
2021-01-21 10:56:37 -0500

    Update package dependency urls to avoid bug in Xcode 12.3

--- Package.swift
+++ Package.swift
@@ -27,11 +27,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/christopherweems/Resultto", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/gitMcFly/MixedGroup", .upToNextMajor(from: "0.0.1")),
-        .package(url: "https://github.com/christopherweems/Statehood", .upToNextMajor(from: "0.0.2")),
-        .package(url: "https://github.com/christopherweems/unstandard", .upToNextMajor(from: "0.0.19")),
+        .package(url: "https://github.com/apple/swift-algorithms.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/christopherweems/Resultto.git", .upToNextMajor(from: "0.0.2")),
+        .package(url: "https://github.com/gitMcFly/MixedGroup.git", .upToNextMajor(from: "0.0.1")),
+        .package(url: "https://github.com/christopherweems/Statehood.git", .upToNextMajor(from: "0.0.2")),
+        .package(url: "https://github.com/christopherweems/unstandard.git", .upToNextMajor(from: "0.0.19")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
